94cbe96a5ded342f837208caf0bd8b12c6251112
kianhawk09 <jimmy.jameskjh@gmail.com>
2026-09-16 10:24:56 +0100

    Added a theme toggle

---
+++ index.js
@@ -0,0 +1,13 @@
+// var button = document.getElementById("theme-toggle");
+// var link = document.getElementById("link");
+
+// function changeTheme(){
+
+//     if (link.getAttribute("href") == "style.css") {
+//         link.setAttribute("href", "styledark.css");
+//     } else {
+//         link.setAttribute("href", "style.css");
+//     }
+// }
+
+// button.onclick = changeTheme();
