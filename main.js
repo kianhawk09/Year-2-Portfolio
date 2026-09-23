389942639f6daaf930782c261ab1fe3bbab87852
kianhawk09 <jimmy.jameskjh@gmail.com>
2026-09-23 10:49:56 +0100

    Added date and Random word

---
+++ main.js
@@ -0,0 +1,35 @@
+const randomWord = document.getElementById("random-word");
+const date = new Date()
+const button = document.getElementById("theme-toggle");
+const link = document.getElementById("link");
+
+var words = ["Cheese", "Tomfoolery", "Purple", "Germany", "Cloud", "Apple","Kangaroo"]
+var word;
+
+function changeTheme(){
+
+    if (link.getAttribute("href") == "style.css") {
+        link.setAttribute("href", "styledark.css");
+          
+    } else {
+      link.setAttribute("href", "style.css");
+    }
+}
+
+for (let i = 0; i < words.length; i++){
+    if (date.getDay() == i){
+        word = words[i];
+    }
+}
+
+//--------------------------------------------------
+
+onload = function() {
+    randomWord.innerHTML = "Random word of the day: " + word;
+    document.getElementById("current-date").innerHTML = "Today's date: " + date.toLocaleDateString();
+}
+
+
+
+
+
