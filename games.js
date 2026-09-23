847bc43a70cb680edb2c51607224bb5d5df768bc
kianhawk09 <jimmy.jameskjh@gmail.com>
2026-09-23 11:06:17 +0100

    Added a games page

---
+++ games.js
@@ -0,0 +1,9 @@
+onload = function() {
+    randomWord.innerHTML = "Random word of the day: " + word;
+    document.getElementById("current-date").innerHTML = "Today's date: " + date.toLocaleDateString();
+}
+
+
+
+
+
